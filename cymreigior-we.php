c9bc314782ad1064f8fe58a1f99d058643458576
emlynjones <emlynjones121@gmail.com>
2015-07-13 17:17:17 +0100

    Creu sgerbwd.
    Sgerbwd Wag.

---
+++ cymreigior-we.php
@@ -0,0 +1,8 @@
+/*
+Plugin Name: Cymreigio'r We
+Plugin URI: Thema.cymru
+Description: Creu Popup pan nad yw'r porwr gwe wedi ei osod i "Gymraeg" a mae'r defnyddiwr yn ymweld a thudalen Gymraeg.
+Version: 0.1
+Author: Gwe Cambrian Web
+Author URI: http://cambrianweb.com
+*/
